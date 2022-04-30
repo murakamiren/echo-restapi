1c52b02b72732d11d6b62160fa2c6b57d8464e65
renmurakami <autoeyy@gmail.com>
2022-04-30 21:18:21 +0900

    getUsers

--- server.go
+++ server.go
@@ -6,11 +6,27 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+type User struct {
+	Id int `json:"id"`
+	Name string `json:"name"`
+	Email string `json:"email"`
+}
+
+func getUsers(c echo.Context) error {
+	Tom := User{1,"Tom","tom@test.com"}
+	John := User{2,"John","john@test.com"}
+	Users := [2]User{Tom,John}
+	return c.JSON(http.StatusOK, Users)
+}
+
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello world")
 	})
 
+	e.GET("/users", getUsers)
+
 	e.Logger.Fatal(e.Start(":1323"))
+
 }
